9f58c468ff009b0fe441e669235c62ffee1946a3
Wojciech_Kubak <wojciech.kubak@smartepc.pl>
2023-10-31 10:29:20 +0100

    CMS-10 ADD: reversed proxy

--- backend/users/users/app.py
+++ backend/users/users/app.py
@@ -1,11 +1,15 @@
+from users.web.configuration import app
 from flask import Flask, Response, make_response
-
-app = Flask(__name__)
+from werkzeug.middleware.proxy_fix import ProxyFix
 
 
 def create_app() -> Flask:
 
     with app.app_context():
+
+        app.wsgi_app = ProxyFix(
+            app.wsgi_app, x_for=1, x_proto=1, x_host=1, x_prefix=0
+        )
 
         @app.route('/')
         def index() -> Response:
