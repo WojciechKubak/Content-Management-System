f8979713cf837e14aa2cb45a19ed3e9974eb60a0
WojciechKubak <wojciechkubak99@gmail.com>
2023-11-03 19:42:49 +0100

    CMS-10 UPDATE: flask app creation

--- backend/users/users/app.py
+++ backend/users/users/app.py
@@ -1,4 +1,9 @@
-from users.routes.user import UserResource, UserListResource, UserRegisterResource, UserActivationResource
+from users.routes.user import (
+    UserResource,
+    UserListResource,
+    UserRegisterResource,
+    UserActivationResource
+)
 from users.security.configure_security import configure_security
 from users.email.configuration import MailConfig
 from users.web.configuration import app
@@ -21,6 +26,7 @@ def create_app() -> Flask:
 
     logging.basicConfig(level=logging.INFO)
     load_dotenv()
+    config = get_config()
 
     with app.app_context():
 
@@ -28,42 +34,53 @@ def create_app() -> Flask:
             app.wsgi_app, x_for=1, x_proto=1, x_host=1, x_prefix=0
         )
 
-        env_config = get_config()
-        app.config.from_object(env_config)
+        app.config.from_object(config)
         sa.init_app(app)
 
-        jwt_settings = {
-            'JWT_COOKIE_SECURE': ast.literal_eval(os.environ.get('JWT_COOKIE_SECURE')),
-            'JWT_TOKEN_LOCATION': ast.literal_eval(os.environ.get('JWT_TOKEN_LOCATION')),
-            'JWT_SECRET_KEY': os.environ.get('JWT_SECRET_KEY'),
-            'JWT_ACCESS_TOKEN_EXPIRES': int(os.environ.get('JWT_ACCESS_TOKEN_EXPIRES')),
-            'JWT_REFRESH_TOKEN_EXPIRES': int(os.environ.get('JWT_REFRESH_TOKEN_EXPIRES')),
-            'JWT_COOKIE_CSRF_PROTECT': ast.literal_eval(os.environ.get('JWT_COOKIE_CSRF_PROTECT'))
-        }
-        app.config.update(jwt_settings)
+        setup_security_envs(app)
         manager = JWTManager(app)
         configure_security(app)
 
-        mail_settings = {
-            'MAIL_SERVER': os.environ.get('MAIL_SERVER'),
-            'MAIL_PORT': int(os.environ.get('MAIL_PORT')),
-            'MAIL_USE_SSL': ast.literal_eval(os.environ.get('MAIL_USE_SSL')),
-            'MAIL_USE_TLS': ast.literal_eval(os.environ.get('MAIL_USE_TLS')),
-            'MAIL_USERNAME': os.environ.get('MAIL_USERNAME'),
-            'MAIL_PASSWORD': os.environ.get('MAIL_PASSWORD')
-        }
-        app.config.update(mail_settings)
+        setup_email_envs(app)
         templates_env = Environment(loader=PackageLoader('users', 'templates'))
         MailConfig.prepare_mail(app, templates_env)
 
-        api = Api(app)
-        api.add_resource(UserListResource, '/users')
-        api.add_resource(UserResource, '/users/<string:username>')
-        api.add_resource(UserActivationResource, '/users/activate')
-        api.add_resource(UserRegisterResource, '/register')
-
-        @app.route('/')
-        def index() -> Response:
-            return make_response({'message': 'Users home page'}, 200)
+        setup_routing(app)
 
         return app
+
+
+def setup_security_envs(flask_app: Flask) -> None:
+    jwt_settings = {
+        'JWT_COOKIE_SECURE': ast.literal_eval(os.environ.get('JWT_COOKIE_SECURE')),
+        'JWT_TOKEN_LOCATION': ast.literal_eval(os.environ.get('JWT_TOKEN_LOCATION')),
+        'JWT_SECRET_KEY': os.environ.get('JWT_SECRET_KEY'),
+        'JWT_ACCESS_TOKEN_EXPIRES': int(os.environ.get('JWT_ACCESS_TOKEN_EXPIRES')),
+        'JWT_REFRESH_TOKEN_EXPIRES': int(os.environ.get('JWT_REFRESH_TOKEN_EXPIRES')),
+        'JWT_COOKIE_CSRF_PROTECT': ast.literal_eval(os.environ.get('JWT_COOKIE_CSRF_PROTECT'))
+    }
+    flask_app.config.update(jwt_settings)
+
+
+def setup_email_envs(flask_app: Flask) -> None:
+    mail_settings = {
+        'MAIL_SERVER': os.environ.get('MAIL_SERVER'),
+        'MAIL_PORT': int(os.environ.get('MAIL_PORT')),
+        'MAIL_USE_SSL': ast.literal_eval(os.environ.get('MAIL_USE_SSL')),
+        'MAIL_USE_TLS': ast.literal_eval(os.environ.get('MAIL_USE_TLS')),
+        'MAIL_USERNAME': os.environ.get('MAIL_USERNAME'),
+        'MAIL_PASSWORD': os.environ.get('MAIL_PASSWORD')
+    }
+    flask_app.config.update(mail_settings)
+
+
+def setup_routing(flask_app: Flask) -> None:
+    @app.route('/')
+    def index() -> Response:
+        return make_response({'message': 'Users home page'}, 200)
+
+    api = Api(flask_app)
+    api.add_resource(UserListResource, '/users')
+    api.add_resource(UserResource, '/users/<string:username>')
+    api.add_resource(UserActivationResource, '/users/activate')
+    api.add_resource(UserRegisterResource, '/register')
