5762a216172a06a0e7a9e6d2d47cd227c17ff7af
WojciechKubak <wojciechkubak99@gmail.com>
2024-01-10 12:25:06 +0100

    CMS-41 ADD: nginx proxy configuration

--- backend/articles/articles/app.py
+++ backend/articles/articles/app.py
@@ -1,3 +1,4 @@
+from werkzeug.middleware.proxy_fix import ProxyFix
 from flask import Flask, Response, make_response
 from dotenv import load_dotenv
 import logging
@@ -6,6 +7,9 @@ app = Flask(__name__)
 
 
 def create_app() -> Flask:
+    app.wsgi_app = ProxyFix(
+        app.wsgi_app, x_for=1, x_proto=1, x_host=1, x_prefix=0
+    )
 
     load_dotenv()
     logging.basicConfig(level=logging.INFO)
