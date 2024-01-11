4f1a9cb4906647c706055c26d842c97ff91e57ff
WojciechKubak <wojciechkubak99@gmail.com>
2024-01-11 20:49:39 +0100

    CMS-49 ADD: domain service calls

---
+++ backend/articles/articles/domain/configuration.py
@@ -0,0 +1,6 @@
+from articles.infrastructure.adapters.configuration import category_db_adapter, article_db_adapter, tag_db_adapter
+from articles.domain.service import CategoryDomainService, ArticleDomainService, TagDomainService
+
+domain_category_service = CategoryDomainService(category_db_adapter)
+domain_article_service = ArticleDomainService(article_db_adapter, category_db_adapter, tag_db_adapter)
+domain_tag_service = TagDomainService(tag_db_adapter)
