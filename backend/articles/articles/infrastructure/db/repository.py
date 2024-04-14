ac6cbed5d2bc07afe6e148499aa1ffbeefba4f96
WojciechKubak <wojciechkubak99@gmail.com>
2024-04-14 13:53:44 +0200

    UPDATE: generic repo

--- backend/articles/articles/infrastructure/db/repository.py
+++ backend/articles/articles/infrastructure/db/repository.py
@@ -1,87 +1,106 @@
 from articles.infrastructure.db.entity import ArticleEntity, CategoryEntity, TagEntity
+from articles.infrastructure.db.entity import Base
 from dataclasses import dataclass
-from sqlalchemy.orm import Session, sessionmaker, scoped_session
+from sqlalchemy.orm import sessionmaker, scoped_session
 from sqlalchemy import Engine
 from typing import Any, Callable
 from functools import wraps
+from abc import ABC, abstractmethod
 
 
 def transactional(method: Callable) -> Callable:
     @wraps(method)
     def wrapper(self, *args: tuple[Any], **kwargs: dict[str, Any]):
-        session = self.session_maker()
+        self.session = self.session_maker()
         try:
-            result = method(self, session, *args, **kwargs)
-            session.commit()
+            result = method(self, *args, **kwargs)
+            self.session.commit()
             return result
         except Exception:
-            session.rollback()
+            self.session.rollback()
             raise
         finally:
-            session.close()
+            self.session.close()
+            self.session = None
 
     return wrapper
 
-
+    
 @dataclass
-class CrudRepository:
-    _engine: Engine
-    _entity: Any
+class CrudRepository[T](ABC):
 
-    def __post_init__(self) -> None:
-        self.session_maker = scoped_session(sessionmaker(self._engine, expire_on_commit=False))
+    @abstractmethod
+    def add_or_update(self, entity: T) -> None:
+        pass
 
-    @transactional
-    def add_or_update(self, session: Session, item: Any) -> Any:
-        return session.merge(item)
+    @abstractmethod
+    def find_by_id(self, entity_id: int) -> T | None:
+        pass
 
-    @transactional
-    def delete(self, session: Session, id_: int) -> None:
-        item = session.query(self._entity).filter_by(id=id_).first()
-        session.delete(item)
+    @abstractmethod
+    def find_all(self) -> list[T]:
+        pass
 
-    @transactional
-    def find_by_id(self, session: Session, id_: int) -> Any | None:
-        return session.query(self._entity).filter_by(id=id_).first()
-
-    @transactional
-    def find_all(self, session: Session) -> list[Any]:
-        return session.query(self._entity).all()
+    @abstractmethod
+    def delete(self, entity_id: int) -> None:
+        pass
 
 
 @dataclass
-class ArticleRepository(CrudRepository):
-    _engine: Engine
-    _entity: Any = ArticleEntity
+class CrudRepositoryORM[T: Base](CrudRepository[T]):
+    engine: Engine
+
+    def __post_init__(self):
+        self.session_maker = scoped_session(
+            sessionmaker(self.engine, expire_on_commit=False)
+        )
+        self.entity = self.__class__.__orig_bases__[0].__args__[0]
 
     @transactional
-    def find_by_title(self, session: Session, title: str) -> ArticleEntity | None:
-        return session.query(self._entity).filter_by(title=title).first()
+    def add_or_update(self, item: Any) -> Any:
+        return self.session.merge(item)
 
     @transactional
-    def find_by_category_id(self, session: Session, category_id: int) -> list[ArticleEntity]:
-        return session.query(self._entity).filter_by(category_id=category_id).all()
+    def delete(self, id_: int) -> None:
+        item = self.session.query(self.entity).filter_by(id=id_).first()
+        self.session.delete(item)
+
+    @transactional
+    def find_by_id(self, id_: int) -> Any | None:
+        return self.session.query(self.entity).filter_by(id=id_).first()
+
+    @transactional
+    def find_all(self) -> list[Any]:
+        return self.session.query(self.entity).all()
 
 
 @dataclass
-class CategoryRepository(CrudRepository):
-    _engine: Engine
-    _entity: Any = CategoryEntity
+class ArticleRepository(CrudRepositoryORM[ArticleEntity]):
 
     @transactional
-    def find_by_name(self, session: Session, name: str) -> CategoryEntity | None:
-        return session.query(self._entity).filter_by(name=name).first()
+    def find_by_title(self, title: str) -> ArticleEntity | None:
+        return self.session.query(self.entity).filter_by(title=title).first()
+
+    @transactional
+    def find_by_category_id(self, category_id: int) -> list[ArticleEntity]:
+        return self.session.query(self.entity).filter_by(category_id=category_id).all()
 
 
 @dataclass
-class TagRepository(CrudRepository):
-    _engine: Engine
-    _entity: Any = TagEntity
+class CategoryRepository(CrudRepositoryORM[CategoryEntity]):
 
     @transactional
-    def find_by_name(self, session: Session, name: str) -> CategoryEntity | None:
-        return session.query(self._entity).filter_by(name=name).first()
+    def find_by_name(self, name: str) -> CategoryEntity | None:
+        return self.session.query(self.entity).filter_by(name=name).first()
+
+
+@dataclass
+class TagRepository(CrudRepositoryORM[TagEntity]):
 
     @transactional
-    def find_many_by_id(self, session: Session, ids: list[int]) -> list[TagEntity]:
-        return session.query(self._entity).filter(self._entity.id.in_(ids)).all()
+    def find_by_name(self, name: str) -> CategoryEntity | None:
+        return self.session.query(self.entity).filter_by(name=name).first()
+
+    @transactional
+    def find_many_by_id(self, ids: list[int]) -> list[TagEntity]:
+        return self.session.query(self.entity).filter(self.entity.id.in_(ids)).all()
