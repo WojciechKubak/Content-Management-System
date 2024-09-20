35a899e38fa5e31da4ef0a705bd38fc2a078b3be
Wojciech Kubak <wojciechkubak99@gmail.com>
2024-09-20 21:28:33 +0200

    create simple model

--- backend/translations/translations/common/models.py
+++ backend/translations/translations/common/models.py
@@ -1,4 +1,4 @@
-from sqlalchemy import func
+from sqlalchemy import String, func
 from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column
 from datetime import datetime
 
@@ -9,7 +9,16 @@ class Base(DeclarativeBase):
 
 class BaseModel(Base):
     __abstract__ = True
+
     created_at: Mapped[datetime] = mapped_column(default=func.utc_timestamp())
     updated_at: Mapped[datetime] = mapped_column(
         default=func.utc_timestamp(), onupdate=func.utc_timestamp()
     )
+
+
+class SimpleModel(BaseModel):
+    __tablename__ = "simple_models"
+
+    id: Mapped[int] = mapped_column(primary_key=True)
+
+    name: Mapped[str] = mapped_column(String(255), nullable=True)
