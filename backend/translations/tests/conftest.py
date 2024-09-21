3d935aabc26227739543eaecc1dafa2c3cee2b4a
Wojciech Kubak <wojciechkubak99@gmail.com>
2024-09-21 12:24:14 +0200

    test s3 integration

--- backend/translations/tests/conftest.py
+++ backend/translations/tests/conftest.py
@@ -3,18 +3,17 @@ from translations.db.configuration import sa
 from translations.config.config import TestingConfig
 from translations.db.repositories import TranslationRepository
 from flask import Flask
-from flask.testing import Client
 from typing import Generator
 import pytest
 
 
 @pytest.fixture(scope="function")
-def app() -> Generator[Flask, None, None]:
+def app() -> Generator:
     yield create_app(TestingConfig)
 
 
 @pytest.fixture(scope="function")
-def client(app: Flask) -> Generator[Client, None, None]:
+def client(app: Flask) -> Generator:
     with app.test_client() as client:
         yield client
 
