4164e87434acf5b0ca30fa1329fdfe41e67b82c5
Wojciech Kubak <wojciechkubak99@gmail.com>
2024-09-23 21:16:59 +0200

    test optimization

--- backend/translations/translations/config/environments/test.py
+++ backend/translations/translations/config/environments/test.py
@@ -1,11 +1,14 @@
 from translations.config.environments.base import *  # noqa
 from translations.enums.enums import TranslationType, StorageType
+from sqlalchemy.pool import NullPool
 
 
 DEBUG: bool = False
 TESTING: bool = True
 
 SQLALCHEMY_DATABASE_URI: str = "mysql://user:user1234@localhost:3309/db_1"
+SQLALCHEMY_ECHO: bool = False
+SQLALCHEMY_ENGINE_OPTIONS = {"poolclass": NullPool}
 
 STORAGE_TYPE_STRATEGY: TranslationType = TranslationType.LOCAL
 TRANSLATION_TYPE_STRATEGY: StorageType = StorageType.LOCAL
