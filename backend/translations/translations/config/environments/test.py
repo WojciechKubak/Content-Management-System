f1e6729cc47a1265f661359a5e1bd52b55291363
Wojciech Kubak <wojciechkubak99@gmail.com>
2024-09-24 18:33:59 +0200

    flask ci cd

--- backend/translations/translations/config/environments/test.py
+++ backend/translations/translations/config/environments/test.py
@@ -1,12 +1,25 @@
 from translations.config.environments.base import *  # noqa
 from translations.enums.enums import TranslationType, StorageType
 from sqlalchemy.pool import Pool, NullPool
+import os
 
 
 DEBUG: bool = False
 TESTING: bool = True
 
 SQLALCHEMY_DATABASE_URI: str = "mysql://user:user1234@localhost:3309/db_1"
+
+if os.environ.get("GITHUB_WORKFLOW"):
+    DB_USER: str = os.environ.get("MYSQL_USER", "user")
+    DB_PASSWORD: str = os.environ.get("MYSQL_PASSWORD", "password")
+    DB_HOST: str = "127.0.0.1"
+    DB_PORT: str = os.environ.get("MYSQL_TCP_PORT", "3306")
+    DB_NAME: str = os.environ.get("MYSQL_DATABASE", "test")
+
+    SQLALCHEMY_DATABASE_URI = (
+        f"mysql://{DB_USER}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{DB_NAME}"
+    )
+
 SQLALCHEMY_ECHO: bool = False
 SQLALCHEMY_ENGINE_OPTIONS: dict[str, Pool] = {"poolclass": NullPool}
 
